2c17e82cad6ecdbe12647afa3d8f7cb7e34ad953
Aseel10-10 <Aseelery1010@gmail.com>
2026-07-25 01:37:28 +0300

    Add home and event pages

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include "includes/header.php"; ?>
+
+<main>
+<h1>Welcome To Our Center!</h1>
+<p>This website was developed to help students discover and register for all campus events.</p>
+
+</main>
+<?php include "includes/footer.php"; ?>
