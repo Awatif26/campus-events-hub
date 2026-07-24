6697a68d2bf650800e8a92293789e10e47679164
Wejdan-22shr <Wejdan22shr@gmail.com>
2026-07-25 02:08:47 +0300

    Add about and events pages

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php include "includes/header.php"; ?>
+
+<main>
+<h1>About Campus Events Hub</h1>
+<p>A website was developed for this center to meet the project requirements for the Web Technologies course.</p>
+</main>
+
+<?php include "includes/footer.php"; ?>
---
+++ events.php
@@ -0,0 +1,8 @@
+<?php include "includes/header.php"; ?>
+
+<main>
+    <h1>Upcoming Events</h1>
+    <p>All events taking place on campus will be listed here in detail.</p>
+</main>
+
+<?php include "includes/footer.php"; ?>
