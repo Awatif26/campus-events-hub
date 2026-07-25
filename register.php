686927bb6ae5186a643b9108759ff343f1de0c1a
Shahadmut-21 <Ghadi7208@gmail.com>
2026-07-25 03:03:12 +0300

    Add registration pages

---
+++ register.php
@@ -0,0 +1,8 @@
+<?php include "includes/header.php"; ?>
+
+<main>
+<h1>Event Registration</h1>
+<p>Here we will provide a form to help students register for events easily.</p>
+</main>
+
+<?php include "includes/footer.php"; ?>
---
+++ registrations.php
@@ -0,0 +1,8 @@
+<?php include "includes/header.php"; ?>
+
+<main>
+<h1>All Registered Students</h1>
+<p>Here, we will display student event registration records.</p>
+</main>
+
+<?php include "includes/footer.php"; ?>
