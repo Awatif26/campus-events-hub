c3afc1b24d9991c1df53e1fef78fd65ce172c57a
Aseel10-10 <Aseelery1010@gmail.com>
2026-07-25 03:50:59 +0300

    Update event page

---
+++ event.php
@@ -0,0 +1,10 @@
+<?php include "includes/header.php"; ?>
+
+<main>
+<h1>Event Details</h1>
+<p>In this section, all information related to the selected event will be displayed.</p>
+</main>
+
+<?php include "includes/footer.php"; ?>
+
+
