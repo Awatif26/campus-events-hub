f4997f890f6819b7edaf022e525995b4ae2f63ff
Awatif@2626 <Awatif26fa@gmail.com>
2026-07-31 18:53:09 +0300

    Add event images

--- data/events.php
+++ data/events.php
@@ -7,7 +7,7 @@ $events = [
         "time" => "8:00 AM",
         "location" => "Department A",
         "category" => "Workshop",
-        "image" => "images/web-development.jpg",
+        "image" => "images/web-development.png",
         "short_description" => "Introduction to the basics of web development.",
         "description" => "This workshop will introduce students to the fundamentals of web development, and they will learn how to use basic programming languages ​​such as CSS, HTML, JS and PHP"
     ],
@@ -19,19 +19,19 @@ $events = [
         "time" => "12:00 PM",
         "location" => "Department B",
         "category" => "Workshop",
-        "image" => "images/artificial-intelligence.jpg",
+        "image" => "images/artificial-intelligence.png",
         "short_description" => "Learn the basic concepts of artificial intelligence.",
         "description" => "In this workshop, students will be introduced to artificial intelligence, its applications, and related technologies such as machine learning, deep learning, and intelligent systems."
     ],
 
     [
         "id" => 3,
-        "title" => "Programming Competition",
+        "title" => "Coding Competition",
         "date" => "2026-08-12",
         "time" => "10:00 AM",
         "location" => "Department C",
         "category" => "Competition",
-        "image" => "images/cybersecurity.jpg",
+        "image" => "images/coding-copm.png",
         "short_description" => "Applying the fundamentals of programming and basic programming languages.",
         "description" => "Participating students will take part in a programming challenges competition that will test their knowledge of programming fundamentals and their ability to handle various scenarios."
     ],
@@ -43,7 +43,7 @@ $events = [
         "time" => "7:00 AM",
         "location" => "University Gate",
         "category" => "Trip",
-        "image" => "images/company-visit.jpg",
+        "image" => "images/company-visit.png",
         "short_description" => "Visiting a technology company to enhance students' experiences and skills.",
         "description" => "A group of students were taken to a technology company for a visit and to learn about the basics of the work environment."
     ]
