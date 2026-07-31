ba1e71948629e84362211d37582b8d2453c1fb39
Asel@1010 <Aseelery1010@gmail.com>
2026-07-31 20:47:14 +0300

    Complete home and event details pages

--- event.php
+++ event.php
@@ -1,10 +1,45 @@
-<?php include "includes/header.php"; ?>
+<?php
+include "data/events.php";
+include "includes/header.php";
+
+$selectedEvent = null;
+if (isset($_GET["id"])) {
+    $eventId = (int) $_GET["id"];
+    foreach ($events as $event) {
+        if ($event["id"] == $eventId) {
+            $selectedEvent = $event;
+            break;
+        }
+    }
+}
+?>
 
 <main>
-<h1>Event Details</h1>
-<p>In this section, all information related to the selected event will be displayed.</p>
+<?php if ($selectedEvent): ?>
+<section class="event-details">
+    <img
+        src="<?= htmlspecialchars($selectedEvent["image"]); ?>"
+        alt="<?= htmlspecialchars($selectedEvent["title"]); ?>"
+    >
+    <h1><?= htmlspecialchars($selectedEvent["title"]); ?></h1>
+    <p><strong class="event-strong">Category:</strong> <?= htmlspecialchars($selectedEvent["category"]); ?></p>
+    <p><strong class="event-strong">Date:</strong> <?= htmlspecialchars($selectedEvent["date"]); ?></p>
+    <p><strong class="event-strong">Time:</strong> <?= htmlspecialchars($selectedEvent["time"]); ?></p>
+    <p><strong class="event-strong">Location:</strong> <?= htmlspecialchars($selectedEvent["location"]); ?></p>
+    <p><?= htmlspecialchars($selectedEvent["description"]); ?></p>
+
+    <a class="register-btn"
+       href="register.php?event=<?= $selectedEvent["id"]; ?>">
+        Register Now
+    </a>
+
+</section>
+
+<?php else: ?>
+<h2>Event not found.</h2>
+<p> The requested event does not exist.
+</p>
+
+<?php endif; ?>
 </main>
-
 <?php include "includes/footer.php"; ?>
-
-
--- index.php
+++ index.php
@@ -1,8 +1,37 @@
-<?php include "includes/header.php"; ?>
+<?php
+include "data/events.php";
+include "includes/header.php";
+?>
 
-<main>
+<main class="main-index">
 <h1>Welcome To Our Center!</h1>
 <p>This website was developed to help students discover and register for all campus events.</p>
+
+<h2>Upcoming Events</h2>
+<section class="events-main">
+    <?php
+    $upcomingEvents = array_slice($events, 0, 2);
+
+    foreach ($upcomingEvents as $event):
+    ?>
+        <div class="event-div">
+            <img
+                src="<?= htmlspecialchars($event["image"]) ?>"
+                alt="<?= htmlspecialchars($event["title"]) ?>"
+            >
+            <h3><?= htmlspecialchars($event["title"]) ?></h3>
+            <p><?= htmlspecialchars($event["date"]) ?></p>
+            <a href="event.php?id=<?= $event["id"] ?>"> More Details </a>
+        </div>
+
+    <?php endforeach; ?>
+</section>
+
+<p>
+    <a class="view-events-btn" href="events.php">
+        View All Events
+    </a>
+</p>
 
 </main>
 <?php include "includes/footer.php"; ?>
