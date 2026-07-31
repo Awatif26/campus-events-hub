d1ea137fcdd457ee6422ec5366f87e70d19c4515
Awatif@2626 <Awatif26fa@gmail.com>
2026-07-31 18:43:39 +0300

    Add events data source

---
+++ data/events.php
@@ -0,0 +1,50 @@
+<?php
+$events = [
+    [
+        "id" => 1,
+        "title" => "Web Development Fundamentals Workshop",
+        "date" => "2026-08-5",
+        "time" => "8:00 AM",
+        "location" => "Department A",
+        "category" => "Workshop",
+        "image" => "images/web-development.jpg",
+        "short_description" => "Introduction to the basics of web development.",
+        "description" => "This workshop will introduce students to the fundamentals of web development, and they will learn how to use basic programming languages ​​such as CSS, HTML, JS and PHP"
+    ],
+
+    [
+        "id" => 2,
+        "title" => "Artificial Intelligence Workshop",
+        "date" => "2026-08-9",
+        "time" => "12:00 PM",
+        "location" => "Department B",
+        "category" => "Workshop",
+        "image" => "images/artificial-intelligence.jpg",
+        "short_description" => "Learn the basic concepts of artificial intelligence.",
+        "description" => "In this workshop, students will be introduced to artificial intelligence, its applications, and related technologies such as machine learning, deep learning, and intelligent systems."
+    ],
+
+    [
+        "id" => 3,
+        "title" => "Programming Competition",
+        "date" => "2026-08-12",
+        "time" => "10:00 AM",
+        "location" => "Department C",
+        "category" => "Competition",
+        "image" => "images/cybersecurity.jpg",
+        "short_description" => "Applying the fundamentals of programming and basic programming languages.",
+        "description" => "Participating students will take part in a programming challenges competition that will test their knowledge of programming fundamentals and their ability to handle various scenarios."
+    ],
+
+    [
+        "id" => 4,
+        "title" => "A trip to a technology company",
+        "date" => "2026-08-20",
+        "time" => "7:00 AM",
+        "location" => "University Gate",
+        "category" => "Trip",
+        "image" => "images/company-visit.jpg",
+        "short_description" => "Visiting a technology company to enhance students' experiences and skills.",
+        "description" => "A group of students were taken to a technology company for a visit and to learn about the basics of the work environment."
+    ]
+];
